99c0850385e10f5ac98ff40df42713f29ccf69f6
Bruno Marques <bruno@bmarques.net>
2021-11-03 19:12:28 +0000

    Add project files.

---
+++ app.js
@@ -0,0 +1,12 @@
+/// <reference path="C:\Program Files\OpenRCT2\openrct2.d.ts" />
+function main() {
+}
+registerPlugin({
+    name: 'Pimba Park',
+    version: '1.0',
+    authors: ['Bruno Marques'],
+    type: 'remote',
+    licence: 'MPL-2.0',
+    main: main
+});
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,14 @@
+/// <reference path="C:\Program Files\OpenRCT2\openrct2.d.ts" />
+
+function main() {
+
+}
+
+registerPlugin({
+  name: 'Pimba Park',
+  version: '1.0',
+  authors: ['Bruno Marques'],
+  type: 'remote',
+  licence: 'MPL-2.0',
+  main: main
+});
